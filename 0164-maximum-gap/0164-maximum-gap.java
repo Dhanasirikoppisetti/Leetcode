82cc10918f56404fc3927e1a3bc834e931e3a47f
Dhanasirikoppisetti <147488134+Dhanasirikoppisetti@users.noreply.github.com>
2025-07-07 22:44:54 +0530

    Time: 40 ms (66.67%), Space: 61.2 MB (25.19%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length<2)return 0;
+        Arrays.sort(nums);
+        int maxgap=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+              int gap=nums[i+1]-nums[i];
+              if(gap>maxgap)
+              {
+                maxgap=gap;
+              }
+        }
+        return maxgap;
+    }
+}
