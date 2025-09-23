c4d67578e0baa9817e647bc2cfcf18d518a701fb
Dhana siri Koppisetti <147488134+Dhanasirikoppisetti@users.noreply.github.com>
2025-09-23 20:15:38 +0530

    Time: 0 ms (100%), Space: 42.1 MB (94.16%) - LeetHub

---
+++ 1310-watering-plants/1310-watering-plants.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+        int steps=0;
+        int water=capacity;
+        for(int i=0;i<plants.length;i++)
+        {
+            if(water<plants[i])
+            {
+                steps+=i*2;
+                water=capacity;
+            }
+            water-=plants[i];
+            steps++;
+        }
+        return steps;
+    }
+}
