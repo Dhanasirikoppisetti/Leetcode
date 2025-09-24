f0b1024f9ba3bcad6e96ab3d59091d0269d32d8c
Dhana siri Koppisetti <147488134+Dhanasirikoppisetti@users.noreply.github.com>
2025-09-24 21:16:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        long n1=Long.parseLong(num1);
+        long n2=Long.parseLong(num2);
+        return String.valueOf(n1*n2);
+    }
+}
